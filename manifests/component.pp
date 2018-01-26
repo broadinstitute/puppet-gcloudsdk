bdf249513dae3f017c81bce9ea0c98a35814211a
Andrew Teixeira <teixeira@broadinstitute.org>
2018-01-26 13:00:46 -0500

    * Convert to PDK and more modern style * Fixed a lot of whitespace inconsistencies * Remove backup copy of README.md * Remove the "agent" script that was sending information to Google Analytics * Rewrite how components are added/removed/updated * Rewrite how gcloud itself is updated * Convert templates to epp

---
+++ manifests/component.pp
@@ -0,0 +1,41 @@
+# @summary Manage a gcloud component's installation status
+#
+# @example
+#    gcloudsdk::component { 'bigtable':
+#      ensure => 'installed',
+#    }
+#
+# @param ensure
+#   The ensure status of the component, either 'absent' or 'installed'
+#
+# @param id
+#   The ID of the component to install
+#
+define gcloudsdk::component (
+  String $component_id                = $name,
+  Enum['absent', 'installed'] $ensure = 'installed',
+){
+  include gcloudsdk
+
+  if $ensure == 'installed' {
+    # The below block of code installs a component if it isn't already installed.
+    exec { "gcloudsdk_component_${component_id}_install":
+      command => "gcloud components install ${component_id}",
+      # Note: gcloud complains if component work happens within the gcloud directory
+      cwd     => '/tmp',
+      path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin', "${gcloudsdk::install_dir}/google-cloud-sdk/bin"],
+      unless  => "gcloud components list --format='csv(ID,Status)' 2>/dev/null | grep -q '^${component_id},Installed'"
+    }
+  }
+
+  if $ensure == 'absent' {
+    # The below block of code remove a component if it is installed.
+    exec { "gcloudsdk_component_${component_id}_remove":
+      command => "gcloud components remove ${component_id}",
+      # Note: gcloud complains if component work happens within the gcloud directory
+      cwd     => '/tmp',
+      path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin', "${gcloudsdk::install_dir}/google-cloud-sdk/bin"],
+      unless  => "gcloud components list --format='csv(ID,Status)' 2>/dev/null | grep -q '^${component_id},Not Installed'"
+    }
+  }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,159 +1,110 @@
-# Class: gcloudsdk
+# @summary Manage a gcloud SDK installation installed directly from Google
 #
-# This module manages gcloudsdk
+# @example
+#    class { 'gcloudsdk':
+#      autoupdate       => true,
+#      bash_completion  => true,
+#      extra_components => {
+#        bigtable => {
+#          ensure => 'installed',
+#        },
+#      },
+#    }
 #
-# Parameters: https://github.com/RanjthKumar45/puppet-gcloudsdk#usage
+# @param autoupdate
+#   Boolean value to determine whether every Puppet run should try to update the
+#   components of the gcloud SDK.
 #
-# Actions: Download and Install Google Cloud Sdk.
+# @param bash_completion
+#   Boolean value to determine whether bash completions are enabled.
 #
-# Requires: puppetlabs/stdlib,camptocamp/archive
+# @param extra_components
+#   A hash of additional components in gcloud to install.
+#   See gcloud::component for example.
 #
-# Sample Usage: https://github.com/RanjthKumar45/puppet-gcloudsdk#usage
+# @param install_dir
+#   The directory to which the SDK will be installed.
+#
+# @param version
+#   The initial version of the SDK to download from Google.
+#
+# @param zsh_completion
+#   Boolean value to determine whether zsh completions are enabled.
 #
 class gcloudsdk (
-  $version = $::gcloudsdk::params::version,
-  $install_dir = $::gcloudsdk::params::install_dir,
-  $is_install_gcloud = $::gcloudsdk::params::is_install_gcloud,
-  $is_install_gsutil = $::gcloudsdk::params::is_install_gsutil,
-  $is_install_core = $::gcloudsdk::params::is_install_core,
-  $is_install_bq = $::gcloudsdk::params::is_install_bq,
-  $is_install_kubectl = $::gcloudsdk::params::is_install_kubectl,
-  $is_install_app_engine_python = $::gcloudsdk::params::is_install_app_engine_python,
-  $is_install_app_engine_java = $::gcloudsdk::params::is_install_app_engine_java,
-  $is_install_beta = $::gcloudsdk::params::is_install_beta,
-  $is_install_alpha = $::gcloudsdk::params::is_install_alpha,
-  $is_install_pubsub_emulator = $::gcloudsdk::params::is_install_pubsub_emulator,
-  $is_install_gcd_emulator = $::gcloudsdk::params::is_install_gcd_emulator,
-  ) inherits ::gcloudsdk::params {
-    
-  #Validate Tools Installation Set up
-  validate_bool($is_install_gcloud)
-  validate_bool($is_install_gsutil)
-  validate_bool($is_install_core)
-  validate_bool($is_install_bq)
-  validate_bool($is_install_kubectl)
-  validate_bool($is_install_app_engine_python)
-  validate_bool($is_install_app_engine_java)
-  validate_bool($is_install_beta)
-  validate_bool($is_install_alpha)
-  validate_bool($is_install_pubsub_emulator)
-  validate_bool($is_install_gcd_emulator)
-  
-  # Validates if the installation directory path exists
-  validate_absolute_path($install_dir)
-  
+  Boolean $autoupdate,
+  Boolean $bash_completion,
+  Hash $extra_components,
+  Stdlib::Absolutepath $install_dir,
+  String $version,
+  Boolean $zsh_completion,
+){
   # Check the Architecture of Node to form the download URL
-  if $::architecture == 'amd64' or $::architecture == 'x86_64' {
+  if ($::architecture == 'amd64') or ($::architecture == 'x86_64') {
     $arch = 'x86_64'
   } else {
     $arch = 'x86'
   }
 
-  # GCloud SDK File Name
-  if $version == 'LATEST' {
-    $download_file_name = "google-cloud-sdk-108.0.0-linux-${arch}"
-  } else {
-    $download_file_name = "google-cloud-sdk-${version}-linux-${arch}"
-  }
-  
+  # gcloud SDK filename
+  $download_file_name = "google-cloud-sdk-${version}-linux-${arch}"
 
-  # GCloud SDK Download URL
+  # gcloud SDK download URL
   $download_source = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/${download_file_name}.tar.gz"
-
-
-
-  notice($download_source)
+  $download_file_path = "/tmp/${download_file_name}.tar.gz"
+  $install_path = "${install_dir}/google-cloud-sdk"
 
   # The below block of code downloads the google-cloud-sdk archive file.
-  archive::download{ "${download_file_name}.tar.gz":
-    url        => $download_source,
-    src_target => '/tmp',
-    checksum   => false,
+  archive { $download_file_path:
+    cleanup      => true,
+    creates      => $install_path,
+    extract      => true,
+    extract_path => $install_dir,
+    source       => $download_source,
   }
-  
-  # The below block of code extracts the google-cloud-sdk archive file.
-  archive::extract { $download_file_name:
-    target     => $install_dir,
-    src_target =>'/tmp',
-    require    => Archive::Download["${download_file_name}.tar.gz"],
-  }
-  
+
   # The below block of code installs the google-cloud-sdk archive file.
-  $install_path = "${install_dir}/google-cloud-sdk"
-  exec { 'install Google Cloud SDK':
-    path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
-    creates => "${install_path}/bin/gcloud",
-    cwd     => "${install_dir}/google-cloud-sdk",
-    command => '/bin/echo "" | ./install.sh --usage-reporting false --disable-installation-options --bash-completion false',
-    require => Archive::Extract[$download_file_name],
+  exec { 'gcloudsdk_install':
+    command     => '/bin/echo "yes" | ./install.sh --usage-reporting false --disable-installation-options --bash-completion false',
+    creates     => "${install_path}/bin/gcloud",
+    cwd         => "${install_dir}/google-cloud-sdk",
+    path        => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+    refreshonly => true,
+    subscribe   => Archive[$download_file_path],
   }
-  
-  
-  
+
   # The below code will set the google-cloud-sdk path inside the PATH env variable.
-  file { '/etc/profile.d/gcloud_path.sh':
+  file { '/etc/profile.d/gcloud.bash.sh':
     ensure  => file,
-    mode    => '0755',
-    content => template('gcloudsdk/gcloud_path.sh.erb'),
-    require => Archive::Extract[$download_file_name],
-  }-> exec { 'set gcloud':
-    provider  => shell,
-    command   => 'sh /etc/profile.d/gcloud_path.sh',
-    logoutput => on_failure,
-  }
-  
-  # The below code will install the selected addtional components. (only if any additional components is set to true)
-  file { '/tmp/sdk_add_components.sh':
-    ensure  => file,
-    mode    => '0755',
-    content => template('gcloudsdk/sdk_add_components.sh.erb'),
-    require => Exec['set gcloud'],
-  }-> exec { 'Add Components':
-    provider  => shell,
-    command   => 'sh /tmp/sdk_add_components.sh',
-    logoutput => on_failure,
-  }
-  
-  # The below code will Update the components to latest version. (only if version is set to LATEST)
-  file { '/tmp/sdk_update_components.sh':
-    ensure  => file,
-    mode    => '0755',
-    content => template('gcloudsdk/sdk_update_components.sh.erb'),
-    require => Exec['Add Components'],
-  }-> exec { 'Update Components':
-    provider  => shell,
-    command   => 'sh /tmp/sdk_update_components.sh',
-    logoutput => on_failure,
-  }
-  
-  # The below code will uninstall the selected default components. (only if default components installation is set to false)
-  file { '/tmp/sdk_remove_components.sh':
-    ensure  => file,
-    mode    => '0755',
-    content => template('gcloudsdk/sdk_remove_components.sh.erb'),
-    require => Exec['Update Components'],
-  }-> exec { 'Remove Components':
-    provider  => shell,
-    command   => 'sh /tmp/sdk_remove_components.sh',
-    logoutput => on_failure,
-  }
-  
-  
-  $ua_module_name = 'RanjithKumar45/gcloudsdk'
-  $ua_module_version = "${ua_module_name}/1.2.0"
-  
-  file { '/tmp/agent.sh':
-    ensure  => file,
-    mode    => '0755',
-    content => template('gcloudsdk/agent.sh.erb'),
-    require => Exec['Remove Components'],
-  }-> exec { 'Agent':
-    provider  => shell,
-    command   => 'sh /tmp/agent.sh',
-    logoutput => on_failure,
+    content => epp('gcloudsdk/gcloud.bash.epp', {
+      bash_completion => $bash_completion,
+      install_path    => $install_path,
+    }),
+    mode    => '0644',
+    require => Archive[$download_file_path],
   }
 
+  # The below code will set the google-cloud-sdk path inside the PATH env variable.
+  file { '/etc/profile.d/gcloud.zsh.sh':
+    ensure  => file,
+    content => epp('gcloudsdk/gcloud.zsh.epp', {
+      zsh_completion => $zsh_completion,
+      install_path   => $install_path,
+    }),
+    mode    => '0644',
+    require => Archive[$download_file_path],
+  }
+
+  # Only run updates if autoupdate is true, and also only if updates are available
+  if $autoupdate {
+    exec { 'gcloudsdk_autoupdate':
+      command => 'echo "Y" | gcloud components update',
+      # Note: gcloud complains if component work happens within the gcloud directory
+      cwd     => '/tmp',
+      path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin', "${gcloudsdk::install_dir}/google-cloud-sdk/bin"],
+      onlyif  => "gcloud components list --format='csv(ID,Status)' 2>/dev/null | grep -q 'Update Available'"
+    }
+  }
+
+  create_resources(gcloudsdk::component, $extra_components)
 }
-
-
-
